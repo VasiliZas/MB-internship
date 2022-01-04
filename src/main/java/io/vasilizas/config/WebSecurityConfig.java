c5d3c3a8a0668ab1a09829ae2a6c88ed89a10286
VasiliZas <vasili.zasinets@gmail.com>
2022-01-04 19:59:05 +0300

    Security oauth2

--- src/main/java/io/vasilizas/config/WebSecurityConfig.java
+++ src/main/java/io/vasilizas/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package io.vasilizas.config;
 
 import io.vasilizas.bean.db.User;
 import io.vasilizas.repositories.jpa.UserRepository;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,24 +17,12 @@ import java.time.LocalDateTime;
 @EnableOAuth2Client
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String LOGIN = "/login";
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .antMatcher("/**")
-//                .authorizeRequests()
-//                .antMatchers(LOGIN, "/error").permitAll()
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage(LOGIN).failureForwardUrl(LOGIN)
-//                .and().oauth2Login()
-//                .defaultSuccessUrl("/home")
-//                .and().logout().logoutSuccessUrl("/").permitAll();
         http.antMatcher("/**")
-        .authorizeRequests()
+                .authorizeRequests()
                 .anyRequest().authenticated()
-                .and()
-                .oauth2Login().defaultSuccessUrl("/home")
+                .and().oauth2Login().defaultSuccessUrl("/home")
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().csrf().disable();
     }
@@ -44,7 +31,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PrincipalExtractor principalExtractor(UserRepository userRepository) {
         return map -> {
             String id = (String) map.get("sub");
-
             User user = userRepository.findById(id).orElseGet(() -> {
                 User newUser = new User();
                 newUser.setId(id);
